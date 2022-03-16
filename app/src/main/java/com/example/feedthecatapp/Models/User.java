8779cb14f03fc6bded25a5b6a451ba902ef0f5fa
darya-cherr <chernikovadarya67@gmail.com>
2022-03-16 23:16:32 +0300

    Set up authorization and registration with Firebase

--- app/src/main/java/com/example/feedthecatapp/Models/User.java
+++ app/src/main/java/com/example/feedthecatapp/Models/User.java
@@ -1,14 +1,14 @@
 package com.example.feedthecatapp.Models;
 
 public class User {
-    private String email;
-    private String password;
+    private String email,name, password;
 
     public User(){
     }
 
-    public User(String email, String password) {
+    public User(String email,String name, String password) {
         this.email = email;
+        this.name = name;
         this.password = password;
     }
 
@@ -18,6 +18,14 @@ public class User {
 
     public void setEmail(String email) {
         this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
     }
 
     public String getPassword() {
--- app/src/main/java/com/example/feedthecatapp/SplashScreen.java
+++ app/src/main/java/com/example/feedthecatapp/SplashScreen.java
@@ -1,14 +1,19 @@
 package com.example.feedthecatapp;
 
+import static android.content.ContentValues.TAG;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
 
 import android.os.Handler;
 import android.text.TextUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.WindowManager;
@@ -19,8 +24,12 @@ import android.widget.EditText;
 import android.widget.ImageView;
 
 import com.example.feedthecatapp.Models.User;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.material.snackbar.Snackbar;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.appcheck.FirebaseAppCheck;
+import com.google.firebase.appcheck.safetynet.SafetyNetAppCheckProviderFactory;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DatabaseReference;
@@ -65,6 +74,11 @@ public class SplashScreen extends AppCompatActivity {
 
         root = findViewById(R.id.rootElement);
 
+        FirebaseApp.initializeApp(this);
+        FirebaseAppCheck firebaseAppCheck = FirebaseAppCheck.getInstance();
+        firebaseAppCheck.installAppCheckProviderFactory(
+                SafetyNetAppCheckProviderFactory.getInstance());
+
         mAuth = FirebaseAuth.getInstance();
         database = FirebaseDatabase.getInstance();
         users = database.getReference("Users");
@@ -79,19 +93,55 @@ public class SplashScreen extends AppCompatActivity {
         authButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                showAuthorizationWindow();
+            }
+        });
+
+    }
+
+    private void showAuthorizationWindow(){
+        AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Authorization");
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View authorizationWnd = inflater.inflate(R.layout.authorization_window,null);
+        dialog.setView(authorizationWnd);
+
+        EditText email = authorizationWnd.findViewById(R.id.emailFieldAuth);
+        EditText password = authorizationWnd.findViewById(R.id.passwordFieldAuth);
+
+        dialog.setPositiveButton("Sign in", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+
+                if(TextUtils.isEmpty(email.getText().toString())){
+                    Snackbar.make(root, "Email field is empty!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.getText().toString().length() < 5){
+                    Snackbar.make(root, "Password length must be more than 5 characters!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+
+                mAuth.signInWithEmailAndPassword(email.getText().toString(), password.getText().toString()).addOnSuccessListener(new OnSuccessListener<AuthResult>() {
+                    @Override
+                    public void onSuccess(AuthResult authResult) {
+                        startActivity(new Intent(getApplicationContext(), MainActivity.class));
+                        finish();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Snackbar.make(root,"Sign In failed. " + e.getMessage(), Snackbar.LENGTH_SHORT);
+                    }
+                });
+
 
             }
         });
 
-        Handler handler = new Handler();
-        handler.postDelayed(new Runnable() {
-            @Override
-            public void run() {
-                /*Intent i = new Intent(getApplicationContext(), MainActivity.class);
-                startActivity(i);
-                finish();*/
-            }
-        }, 4000);
+
+        dialog.show();
     }
 
     private void showRegisterWindow(){
@@ -102,18 +152,24 @@ public class SplashScreen extends AppCompatActivity {
         View registerWnd = inflater.inflate(R.layout.register_window,null);
         dialog.setView(registerWnd);
 
-        EditText email = registerWnd.findViewById(R.id.emailField);
-        EditText password = registerWnd.findViewById(R.id.passwordField);
+        EditText email = registerWnd.findViewById(R.id.emailFieldReg);
+        EditText password = registerWnd.findViewById(R.id.passwordFieldReg);
+        EditText name = registerWnd.findViewById(R.id.nameField);
 
         dialog.setPositiveButton("Sign up", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialogInterface, int i) {
+
                 if(TextUtils.isEmpty(email.getText().toString())){
-                    Snackbar.make(root, "Login field is empty!", Snackbar.LENGTH_SHORT).show();
+                    Snackbar.make(root, "Email field is empty!", Snackbar.LENGTH_SHORT).show();
                     return;
                 }
-                if(password.getText().toString().length() < 5){
-                    Snackbar.make(root, "Password length must be more than 5 characters!", Snackbar.LENGTH_SHORT).show();
+                if(TextUtils.isEmpty(name.getText().toString())){
+                    Snackbar.make(root, "Name field is empty!", Snackbar.LENGTH_SHORT).show();
+                    return;
+                }
+                if(password.getText().toString().length() < 6){
+                    Snackbar.make(root, "Password length must be more than 5 characters!", Snackbar.LENGTH_LONG).show();
                     return;
                 }
 
@@ -121,13 +177,15 @@ public class SplashScreen extends AppCompatActivity {
                     @Override
                     public void onSuccess(AuthResult authResult) {
                         User user = new User();
+                        System.out.print("user created");
                         user.setEmail(email.getText().toString());
+                        user.setName(name.getText().toString());
                         user.setPassword(password.getText().toString());
 
-                        users.child(user.getEmail()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
+                        users.child(FirebaseAuth.getInstance().getCurrentUser().getUid()).setValue(user).addOnSuccessListener(new OnSuccessListener<Void>() {
                             @Override
                             public void onSuccess(Void unused) {
-                                Snackbar.make(root, "You have successfully registered", Snackbar.LENGTH_SHORT).show();
+                                Snackbar.make(root, "You have successfully registered", Snackbar.LENGTH_LONG).show();
                             }
                         });
                     }
